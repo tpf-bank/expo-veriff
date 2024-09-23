e37da6669fa1085ffd8e8eb3283e9432e226fffb
hgrj <grhgjr@jgrkg.gr>
2024-09-23 17:24:43 +0500

    0.1.1

--- app.plugin.js
+++ app.plugin.js
@@ -4,63 +4,23 @@ const {
   withProjectBuildGradle,
 } = require('@expo/config-plugins');
 
-function getUpdatedProjectGradle(buildGradle) {
-  const insertIndex = getInsertIndex(buildGradle);
-
-  // Skip adding maven url if no insertion points found
-  if (insertIndex !== -1) {
-    const updatedBuildGradle = [
-      buildGradle.slice(0, insertIndex),
-      "maven { url 'https://cdn.veriff.me/android/' } //veriff\n        ",
-      buildGradle.slice(insertIndex),
-    ].join('');
-
-    return updatedBuildGradle;
-  }
-  return buildGradle;
-}
-
-function getInsertIndex(buildGradle) {
-  // legacy logic, left for backward compatability
-  const mavenLocalIndex = buildGradle.indexOf('mavenLocal()');
-  if (mavenLocalIndex !== -1) {
-    console.log(`found "mavenLocal()" at index ${mavenLocalIndex}`);
-    return mavenLocalIndex;
-  }
-
-  // logic to support new Expo build.gradle file structure
-  const allprojectsIndex = buildGradle.indexOf('allprojects');
-  if (allprojectsIndex !== -1) {
-    console.log(`found "allprojects" at index ${allprojectsIndex}`);
-    // repositories after or "inside" of allprojects
-    const repositoriesIndex = buildGradle.indexOf(
-      'repositories',
-      allprojectsIndex,
-    );
-    if (repositoriesIndex !== -1) {
-      console.log(`found "repositories" at index ${repositoriesIndex}`);
-      // mavenCentral() after or "inside" of repositories
-      const mavenCentralIndex = buildGradle.indexOf(
-        'mavenCentral()',
-        repositoriesIndex,
-      );
-      if (mavenCentralIndex !== -1) {
-        console.log(`found "mavenCentral()" at index ${mavenCentralIndex}`);
-        return mavenCentralIndex;
-      }
-    }
-  }
-
-  console.log('mavenLocal() or mavenCentral() not found!');
-
-  return -1;
-}
+const lines = [
+  '\n// @generated begin expo-veriff -  prebuild (DO NOT MODIFY)',
+  'allprojects {',
+  '    repositories { maven { url "https://cdn.veriff.me/android/" } }',
+  '    configurations {',
+  '        all*.exclude module: \'bcprov-jdk15to18\'',
+  '        all*.exclude module: \'bcutil-jdk18on\'',
+  '        all*.exclude module: \'bcprov-jdk15on\'',
+  '        all*.exclude module: \'bcutil-jdk15on\'',
+  '    }',
+  '}',
+  '// @generated end expo-veriff'
+].join("\n")
 
 function withVeriffProjectGradle(expoConfig) {
   return withProjectBuildGradle(expoConfig, (config) => {
-    config.modResults.contents = getUpdatedProjectGradle(
-      config.modResults.contents,
-    );
+    config.modResults.contents = config.modResults.contents + lines
     return config;
   });
 }
